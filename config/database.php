5c90db547fbbc9c6fba34cbfb508693eb139c745
jarmandoR <jose523a@gmail.com>
2026-03-30 12:33:54 -0500

    wip se cambian variables de conexion a produccion

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 return array(
     'host' => 'localhost',
-    'database' => 'bgo',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'u885436177_registroUsu',
+    'username' => 'u885436177_registroUsu',
+    'password' => 'Americas2O2G',
     'charset' => 'utf8',
 );
